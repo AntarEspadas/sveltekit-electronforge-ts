49a1f5b60bf6b796c2543edde516acf1e2d622f7
Naratna <antar.espadas@hotmail.com>
2022-08-01 14:27:12 -0500

    Moved config to separate file

---
+++ forge.config.cjs
@@ -0,0 +1,33 @@
+const name = require("./package.json").name
+
+module.exports = {
+    packagerConfig: {
+        executableName: name
+    },
+    makers: [
+        {
+            name: "@electron-forge/maker-squirrel",
+            config: {
+                authors: "YOU",
+                exe: name
+            },
+            platforms: [
+                "linux"
+            ]
+        },
+        {
+            name: "@electron-forge/maker-zip",
+            platforms: [
+                "darwin"
+            ]
+        },
+        {
+            name: "@electron-forge/maker-deb",
+            config: {}
+        },
+        {
+            name: "@electron-forge/maker-rpm",
+            config: {}
+        }
+    ]
+}
